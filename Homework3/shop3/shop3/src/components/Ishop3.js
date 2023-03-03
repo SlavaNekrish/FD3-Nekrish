77c6d844c9f6ff70c28dc5852e4d4c91d8c95385
SlavaNekrish <vnekrish@mail.ru>
2023-03-03 20:11:54 +0300

    edit form finished

--- Homework3/shop3/shop3/src/components/Ishop3.js
+++ Homework3/shop3/shop3/src/components/Ishop3.js
@@ -24,47 +24,98 @@ class Ishop3 extends React.Component {
     selectedGood: null,
     isEditing: false,
     editingCardCode: null,
-    nameValue: '',
-    priceValue: '',
-    urlValue: '',
-    quantValue: '',
+    nameValue: '', priceValue: '',
+    urlValue: '', quantValue: '',
     disableButtons: false,
+    emptyNameError: false, emptyPriceError: false, emptyURLError: false, emptyQuantError: false, 
+    notStringNameError: false, notStringURLError: false,
+    notNumberPriceError: false, notNumberQuantError: false,
+    disableSaveBut: false,
+    dontShowModal: ''
   };
 
   GoodSelected = (code) => {
     this.setState( {selectedGood:code} );
+
   };
 
   GoodEdited = (code) => {
-    this.setState( {isEditing: true, editingCardCode: code} );
+    let editedCard = this.state.goodsList.filter(n => n.code === code);
+    this.setState( {isEditing: true, previousGood: editedCard, editingCardCode: code, nameValue: editedCard[0].name,
+      priceValue: editedCard[0].price, urlValue: editedCard[0].url, quantValue: editedCard[0].count} );
     this.GoodSelected (code)
   };
 
   inputNameChange = (event) => {
-    this.setState( {nameValue: event.target.value}, this.buttonsDisabling());
+    this.setState( {nameValue: event.target.value} );
   }
 
   inputPriceChange = (event) => {
-    this.setState( {priceValue: event.target.value}, this.buttonsDisabling() );
+    this.setState( {priceValue: event.target.value} );
   }
 
   inputURLChange = (event) => {
-    this.setState( {urlValue: event.target.value}, this.buttonsDisabling() );
+    this.setState( {urlValue: event.target.value} );
   }
 
   inputQuantChange = (event) => {
-    this.setState( {quantValue: event.target.value}, this.buttonsDisabling() );
+    this.setState( {quantValue: event.target.value} );
   }
 
-  //разобраться , почему buttonsDisabling не отрабатывает корректно
-
   buttonsDisabling = () => {
-    (this.state.nameValue !== '' || this.state.priceValue !== '' ||
-    this.state.urlValue !== '' || this.state.quantValue !== '') &&
-    this.setState( {disableButtons: true} );
-    (this.state.nameValue === '' && this.state.priceValue === '' &&
-    this.state.urlValue === '' && this.state.quantValue === '') &&
-    this.setState( {disableButtons: false} );
+    this.setState( {disableButtons: true, dontShowModal: "dontShowModal"} );
+  }
+
+  buttonSaveDisabling = () => {
+    ( this.state.emptyNameError ||  this.state.emptyPriceError || this.state.emptyURLError || this.state.emptyQuantError || 
+      this.state.notStringNameError || this.state.notStringURLError || this.state.notNumberPriceError || this.state.notNumberQuantError ) 
+      && this.setState( {disableSaveBut: true} );
+    ( !this.state.emptyNameError && !this.state.emptyPriceError && !this.state.emptyURLError && !this.state.emptyQuantError && 
+      !this.state.notStringNameError && !this.state.notStringURLError && !this.state.notNumberPriceError && !this.state.notNumberQuantError ) 
+      && this.setState( {disableSaveBut: false} );
+  }
+
+  createError = (event) => {
+    switch (event.target.className) {
+      case "Name":
+        (event.target.value === '') ? 
+        this.setState( {emptyNameError: true}, this.buttonSaveDisabling) : this.setState( {emptyNameError: false}, this.buttonSaveDisabling);  
+        (Number(event.target.value) || event.target.value === '') ?
+        this.setState( {notStringNameError: true}, this.buttonSaveDisabling ) : this.setState( {notStringNameError: false}, this.buttonSaveDisabling );  
+      break;
+      case "Price":
+        (event.target.value === '') ? 
+        this.setState( {emptyPriceError: true}, this.buttonSaveDisabling ) : this.setState( {emptyPriceError: false}, this.buttonSaveDisabling );  
+        (!Number(event.target.value) || event.target.value === '') ?
+        this.setState( {notNumberPriceError: true}, this.buttonSaveDisabling ) : this.setState( {notNumberPriceError: false}, this.buttonSaveDisabling );  
+      break;
+      case "URL":
+        (event.target.value === '') ? 
+        this.setState( {emptyURLError: true}, this.buttonSaveDisabling ) : this.setState( {emptyURLError: false}, this.buttonSaveDisabling );  
+        (Number(event.target.value) || event.target.value === '') ?
+        this.setState( {notStringURLError: true}, this.buttonSaveDisabling ) : this.setState( {notStringURLError: false}, this.buttonSaveDisabling );  
+      break;
+      case "Quantity":
+        (event.target.value === '') ? 
+        this.setState( {emptyQuantError: true}, this.buttonSaveDisabling ) : this.setState( {emptyQuantError: false}, this.buttonSaveDisabling );  
+        (!Number(event.target.value) || event.target.value === '') ?
+        this.setState( {notNumberQuantError: true}, this.buttonSaveDisabling ) : this.setState( {notNumberQuantError: false}, this.buttonSaveDisabling );  
+      break;
+    }
+  }
+
+  saveEditing = () => {
+    const editedGood = { name: this.state.nameValue, code: this.state.editingCardCode,
+            price: Number(this.state.priceValue), url: this.state.urlValue, 
+            count: Number(this.state.quantValue) };
+    const oldGoodIndex = this.state.goodsList.findIndex(el => el.name === this.state.nameValue)
+    this.state.goodsList.splice(oldGoodIndex, 1, editedGood)
+    this.setState( {goodsList: this.state.goodsList, isEditing: false, disableButtons: false, dontShowModal: '', 
+    selectedGood: null, editingCardCode: null} );
+  }
+
+  cancelEditing = () => {
+    this.setState( {isEditing: false, disableButtons: false, dontShowModal: '', selectedGood: null} );
   }
 
   deleteThisGood = (code) => {
@@ -88,13 +139,14 @@ class Ishop3 extends React.Component {
     );
 
     let modalCard=this.state.goodsList.filter(n => n.code === this.state.selectedGood).map( v =>
-      <div className='ModalWrapper' key={v.code}>
-        <h3>{v.name}</h3>
-        <img src={v.url} alt='modalPicture'/>
-        <h5>Price: {v.price}</h5>
+      <div className={this.state.dontShowModal} key={v.code}>
+        <div className='ModalWrapper'>
+          <h3>{v.name}</h3>
+          <img src={v.url} alt='modalPicture'/>
+          <h5>Price: {v.price}</h5>
+        </div>
       </div>
     )
-
     
     return (
       <div className='Wrapper'>
@@ -116,31 +168,39 @@ class Ishop3 extends React.Component {
           {(!this.state.isEditing) && <input type='button' value='New product'className='NewProduct' />}
         </div>
         <div>
-          {(!this.state.isEditing) &&  modalCard}
+          {(this.state.editingCardCode !== this.state.selectedGood) && modalCard}
         </div>
          {
            (this.state.isEditing) && 
-           <form>
+           <form onChange={this.buttonsDisabling}>
             <h3>Edit existing Good</h3>
               <span>ID: {this.state.editingCardCode}</span><br />
               <label>
                 Name:
-                <input type='text' value={this.state.nameValue} onChange={this.inputNameChange}/>
+                <input type='text' value={this.state.nameValue} className='Name fr' onChange={this.inputNameChange} onBlur={this.createError}/>
+                {(this.state.emptyNameError) &&  <span className='Error'>Please, fill the field</span>}
+                {(this.state.notStringNameError) &&  <span className='Error'>  Value must be a string</span>}
               </label><br />
               <label>
                 Price:
-                <input type='text' value={this.state.priceValue} onChange={this.inputPriceChange}/>
+                <input type='text' value={this.state.priceValue} className='Price fr' onChange={this.inputPriceChange} onBlur={this.createError}/>
+                {(this.state.emptyPriceError) &&  <span className='Error'>Please, fill the field</span>}
+                {(this.state.notNumberPriceError) &&  <span className='Error'>  Value must be a number</span>}
               </label><br />
               <label>
                 URL:
-                <input type='text' value={this.state.urlValue} onChange={this.inputURLChange}/>
+                <input type='text' value={this.state.urlValue} className='URL fr' onChange={this.inputURLChange} onBlur={this.createError}/>
+                {(this.state.emptyURLError) &&  <span className='Error'>Please, fill the field</span>}
+                {(this.state.notStringURLError) &&  <span className='Error'>  Value must be a string</span>}
               </label><br />
               <label>
                 Quantity:
-                <input type='text' value={this.state.quantValue} onChange={this.inputQuantChange}/>
+                <input type='text' value={this.state.quantValue} className='Quantity fr' onChange={this.inputQuantChange} onBlur={this.createError}/>
+                {(this.state.emptyQuantError) &&  <span className='Error'>Please, fill the field</span>}
+                {(this.state.notNumberQuantError) &&  <span className='Error'>  Value must be a number</span>}
               </label><br />
-              <input type='button' value='Save' />
-              <input type='button' value='Cancel' />
+              <input type='button' value='Save'  disabled={this.state.disableSaveBut} onClick={this.saveEditing}/>
+              <input type='button' value='Cancel' onClick={this.cancelEditing}/>
            </form>
          }
       </div>
