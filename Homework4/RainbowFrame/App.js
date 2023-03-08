cb5426c1933cd776dd7a3a738810d6bc2dcac59b
SlavaNekrish <vnekrish@mail.ru>
2023-03-08 22:45:45 +0300

    RainbowFrame done

---
+++ Homework4/RainbowFrame/App.js
@@ -0,0 +1,15 @@
+﻿import React from 'react';
+import ReactDOM from 'react-dom';
+
+import RainbowFrame from './components/RainbowFrame.js';
+
+
+let colorsArr=require('./colors.json');
+
+ReactDOM.render(
+  <RainbowFrame colors={colorsArr}>
+    Hello!
+  </RainbowFrame>
+  , document.getElementById('container') 
+);
+
---
+++ Homework4/RainbowFrame/components/RainbowFrame.js
@@ -0,0 +1,29 @@
+﻿import React from 'react';
+import PropTypes from 'prop-types';
+
+class RainbowFrame extends React.Component {
+
+    static propTypes = {
+        colors: PropTypes.array,
+    };
+
+    render() {
+        const reduceCB = (acc, cur) => {
+            return (
+                <div style={{ border: "solid 5px " + cur, padding: "5px" }}>
+                    {acc}
+                </div>
+            )
+        }
+
+        const rainbowFrame = this.props.colors.reduce(reduceCB, this.props.children);
+
+        return (
+            <div style={{ width: 300, textAlign: 'center', margin: '100px auto' }}>
+                {rainbowFrame}
+            </div>
+        );
+    }
+}
+
+export default RainbowFrame;
