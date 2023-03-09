402587fe2853129399dd791a9b07f977d6f6450b
SlavaNekrish <vnekrish@mail.ru>
2023-03-09 23:22:21 +0300

    br2jsx for check

---
+++ Homework4/br2jsx/App.js
@@ -0,0 +1,12 @@
+﻿import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Br2jsx from './components/br2jsx.js';
+
+ReactDOM.render(
+  <Br2jsx>
+    {"первый<br>второй<br/>третий<br />последний"}
+  </Br2jsx>
+  , document.getElementById('container') 
+);
+
---
+++ Homework4/br2jsx/components/br2jsx.js
@@ -0,0 +1,27 @@
+﻿import React from 'react';
+import PropTypes from 'prop-types';
+
+class Br2jsx extends React.Component {
+
+    static propTypes = {
+        children: PropTypes.string.isRequired
+    };
+
+    render() {
+        const mapCB = el => {
+            return (
+                  <React.Fragment>{el}<br/></React.Fragment>
+            )
+        }
+
+        const mineContent = this.props.children.split(/<br\s*\/*>/).map(mapCB);
+        
+        return (
+            <div className='br2jsx' style={{ width: 200, padding: '20px 0px',textAlign: 'center', margin: '100px auto', border: "solid 1px black", backgroundColor: 'lightblue'}}>
+                {mineContent}
+            </div>
+        );
+    }
+}
+
+export default Br2jsx;
