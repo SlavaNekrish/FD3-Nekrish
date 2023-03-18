b2c8c4aa36699bc0c7fe3fe5eade35647592f9c2
SlavaNekrish <vnekrish@mail.ru>
2023-03-18 16:29:06 +0300

    mobile for check

--- Homework5/Mobile/components/MobileClient.js
+++ Homework5/Mobile/components/MobileClient.js
@@ -8,19 +8,18 @@ import './MobileClient.css';
 class MobileClient extends React.PureComponent {
 
   static propTypes = {
-    id: PropTypes.number.isRequired,
-    FIO:PropTypes.shape({
+    client:PropTypes.shape({
       fam: PropTypes.string.isRequired,
       im: PropTypes.string.isRequired,
       otch: PropTypes.string.isRequired,
+      id: PropTypes.number.isRequired,
+      balance: PropTypes.number.isRequired,
     }),
-    balance: PropTypes.number.isRequired,
     workMode: PropTypes.number.isRequired,
   };
 
   state = {
-    FIO: this.props.FIO,
-    balance: this.props.balance,
+    client: this.props.client,
     workMode: this.props.workMode,
   };
 
@@ -30,42 +29,45 @@ class MobileClient extends React.PureComponent {
   newBalance = React.createRef();
 
   componentDidUpdate = (oldProps, oldState) => {
-    console.log("MobileClient id="+this.props.id+" componentDidUpdate");
-    if ( this.props.balance!==this.state.balance || this.props.FIO!==this.state.FIO)
-      this.setState({balance:this.props.balance, FIO: this.props.FIO});
+    console.log("MobileClient id="+this.props.client.id+" componentDidUpdate");
+    if ( this.props.client.balance!== this.state.client.balance || this.props.client.fam !== this.state.client.fam || 
+      this.props.client.im !== this.state.client.im || this.props.client.otch !== this.state.client.otch || 
+      this.props.client.id !== this.state.client.id)
+      this.setState({client:this.props.client});
   };
 
   theClientEdited = (EO) => {
     const elem = {
-      id: this.props.id,
-      FIO: {
-          fam: this.newFam.current ? this.newFam.current.value : this.props.FIO.fam,
-          im: this.newIm.current ? this.newIm.current.value : this.props.FIO.im,
-          otch: this.newOtch.current ? this.newOtch.current.value : this.props.FIO.otch,
-      },
-      balance: this.newBalance.current ? +this.newBalance.current.value : this.props.balance,
+          fam: this.newFam.current ? this.newFam.current.value : this.props.client.fam,
+          im: this.newIm.current ? this.newIm.current.value : this.props.client.im,
+          otch: this.newOtch.current ? this.newOtch.current.value : this.props.client.otch,
+          balance: this.newBalance.current ? +this.newBalance.current.value : this.props.client.balance,
+          id: this.props.client.id,
   }
-    console.log(elem);
     mobileEvents.emit('ClientEdited', elem);
 
     this.setState({workMode: 1})
   };
 
+  theClientDeleted = (EO) => {
+    mobileEvents.emit('ClientDeleted', this.props.client.id);
+  }
+
   render() {
 
-    console.log("MobileClient id="+this.props.id+" render");
+    console.log("MobileClient id="+this.props.client.id+" render");
      
     return (
   
         <tr className='MobileClient'>
           { ( this.state.workMode==1 ) &&
           <React.Fragment>
-            <th className='column'>{this.state.FIO.fam}</th>
-            <th className='2column'>{this.state.FIO.im}</th>
-            <th className='3column'>{this.state.FIO.otch}</th>
-            <th className='4column'>{this.state.balance}</th>
+            <th className='column'>{this.state.client.fam}</th>
+            <th className='2column'>{this.state.client.im}</th>
+            <th className='3column'>{this.state.client.otch}</th>
+            <th className='4column'>{this.state.client.balance}</th>
             {
-              (this.state.balance>=0)
+              (this.state.client.balance>=0)
               ? <th className='5column MobileClientBalanceActive'><span>active</span></th>
               : <th className='5column MobileClientBalanceBlocked'><span>blocked</span></th>
             }
@@ -73,22 +75,22 @@ class MobileClient extends React.PureComponent {
             <input type='button' value='Редактировать' onClick={() => {this.setState({workMode: 0})}}/>
             </th>
             <th className='7column'>
-            <input type='button' value='Удалить' />
+            <input type='button' value='Удалить' onClick={this.theClientDeleted}/>
             </th>
           </React.Fragment>
           } 
           { ( this.state.workMode==0 ) && 
           <React.Fragment>
-            <th className='column'> <input type='text' ref={this.newFam} defaultValue={this.props.FIO.fam}/></th>
-            <th className='2column'><input type='text' ref={this.newIm} defaultValue={this.props.FIO.im}/></th>
-            <th className='3column'><input type='text' ref={this.newOtch} defaultValue={this.props.FIO.otch}/></th>
-            <th className='4column'><input type='text' ref={this.newBalance} defaultValue={this.props.balance} /></th>
+            <th className='column'> <input type='text' ref={this.newFam} defaultValue={this.props.client.fam}/></th>
+            <th className='2column'><input type='text' ref={this.newIm} defaultValue={this.props.client.im}/></th>
+            <th className='3column'><input type='text' ref={this.newOtch} defaultValue={this.props.client.otch}/></th>
+            <th className='4column'><input type='text' ref={this.newBalance} defaultValue={this.props.client.balance} /></th>
             <th className='5column'>----</th>
             <th className='6column'>
             <input type='button' value='Редактировать' onClick={this.theClientEdited}/>
             </th>
             <th className='7column'>
-            <input type='button' value='Удалить' />
+            <input type='button' value='Удалить' onClick={this.theClientDeleted}/>
             </th>
           </React.Fragment>
           }
--- Homework5/Mobile/components/MobileCompany.js
+++ Homework5/Mobile/components/MobileCompany.js
@@ -23,58 +23,97 @@ class MobileCompany extends React.PureComponent {
 
   state = {
     clients: this.props.clients,
+    addingForm: false,
+    status: 0,
   };
+
+  addFam = React.createRef();
+  addIm = React.createRef();
+  addOtch = React.createRef();
+  addBalance = React.createRef();
 
   componentDidMount = () => {
     mobileEvents.addListener('ClientEdited', this.editClient);
+    mobileEvents.addListener('ClientDeleted', this.deleteClient);
   };
 
   componentWillUnmount = () => {
     mobileEvents.removeListener('ClientEdited', this.editClient);
+    mobileEvents.removeListener('ClientDeleted', this.deleteClient);
   };
 
   editClient = (elem) => {
-    console.log(elem);
     let changed=false;
     let newClients=[...this.state.clients]; // копия самого массива клиентов
     newClients.forEach( (c,i) => {
-      if ( c.id==elem.id && (c.balance!=elem.balance || c.fam!=elem.FIO.fam || c.im!=elem.FIO.im || c.otch!=elem.FIO.otch)) {
+      if ( c.id==elem.id && (c.balance!=elem.balance || c.fam!=elem.fam || c.im!=elem.im || c.otch!=elem.otch)) {
         let newClient={...c}; // копия хэша изменившегося клиента
         newClient.balance=elem.balance;
-        newClient.fam=elem.FIO.fam;
-        newClient.im=elem.FIO.im;
-        newClient.otch=elem.FIO.otch;
+        newClient.fam=elem.fam;
+        newClient.im=elem.im;
+        newClient.otch=elem.otch;
         newClient.id=elem.id;
         newClients[i]=newClient;
-        console.log(newClient);
         changed=true;
       }
     } );
     if ( changed )
-      console.log(newClients);
       this.setState({clients:newClients});
-      console.log(this.state.clients)
-  }
+  };
 
+  deleteClient = (clientid) => {
+    let changed=false;
+    let newClients=[...this.state.clients]; // копия самого массива клиентов
+    newClients = newClients.filter(n => n.id !== clientid)
+    changed=true;
+    if ( changed )
+    this.setState({clients:newClients});
+  };
+
+  addClient = (EO) => {
+    let changed=false;
+    let newClients=[...this.state.clients]; // копия самого массива клиентов
+    const newClient={
+      balance: this.addBalance.current ? +this.addBalance.current.value : null,
+      fam: this.addFam.current ? this.addFam.current.value : null,
+      im: this.addIm.current ? this.addIm.current.value : null,
+      otch: this.addOtch.current ? this.addOtch.current.value : null,
+      id: this.state.clients.reduce((acc, cur) => acc.id > cur.id ? acc.id : cur.id) + 1,
+    }
+    newClients.push(newClient);
+    changed=true;
+    if ( changed )
+    this.setState({clients:newClients, addingForm: false});
+  };
+
+  filterAll = () => this.setState({ status: 0 });
+  filterActive = () => this.setState({ status: 1 });
+  filterBlocked = () => this.setState({ status: 2 });
+  
   render() {
 
     console.log("MobileCompany render");
 
-    const clientsCode=this.state.clients.map( client => {
-        let FIO={fam:client.fam,im:client.im,otch:client.otch};
+    const filteredClients = this.state.clients.filter((client) => {
+      switch (this.state.status) {
+        case 1: return client.balance > 0;
+        case 2: return client.balance < 0;
+        default: return true;
+      }
+    });
+    
+    const clientsCode=filteredClients.map( client => {
         return <MobileClient key={client.id} 
-          id={client.id} 
-          FIO={FIO} 
-          balance={client.balance}
+          client={client}
           workMode={this.props.workMode} />;
       }
     );
 
     return (
       <div className='MobileCompany'>
-          <input type="button" value="Все" />
-          <input type="button" value="Активные" />
-          <input type="button" value="Заблокированные" />
+          <input type="button" value="Все" onClick={this.filterAll}/>
+          <input type="button" value="Активные" onClick={this.filterActive}/>
+          <input type="button" value="Заблокированные" onClick={this.filterBlocked} />
           <table className='MobileTable'>
           <thead>
               <tr className='tableHeader'>
@@ -89,9 +128,19 @@ class MobileCompany extends React.PureComponent {
             </thead>
             <tbody className='allClients'>
             {clientsCode}
+            { ( this.state.addingForm ) && 
+              <tr>
+                <th className='column'> <input type='text' ref={this.addFam} /></th>
+                <th className='2column'><input type='text' ref={this.addIm} /></th>
+                <th className='3column'><input type='text' ref={this.addOtch} /></th>
+                <th className='4column'><input type='text' ref={this.addBalance} /></th>
+                <th><input type="button" value="Готово" onClick={this.addClient} /></th>
+              </tr>
+            }
             </tbody>
           </table>
-          <input type="button" value="Добавить клиента" />
+          <input type="button" value="Добавить клиента" onClick={()=>this.setState({addingForm: true})}/>
+          
       </div>
     )
     ;
