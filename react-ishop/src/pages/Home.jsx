471ce4636fe5b6ba54b51cf250f18c1e01d99907
SlavaNekrish <vnekrish@mail.ru>
2023-04-04 18:16:05 +0300

    Search & Context added

--- react-ishop/src/pages/Home.jsx
+++ react-ishop/src/pages/Home.jsx
@@ -4,11 +4,16 @@ import Categories from '../components/Categories';
 import Sort from '../components/Sort';
 import PizzaBlock from '../components/PizzaBlock';
 import Skeleton from '../components/PizzaBlock/Skeleton';
+import Pagination from '../components/Pagination';
+import { SearchContext } from '../App';
 
 const Home = () => {
+  const { searchValue } = React.useContext(SearchContext);
+
   const [items, setItems] = useState([]);
   const [isLoading, setLoading] = useState(true);
   const [categoryId, setCategoryId] = useState(0);
+  const [currentPage, setcurrentPage] = useState(1);
   const [sortType, setsortType] = useState({
     name: 'популярности DESC',
     code: 0,
@@ -21,9 +26,10 @@ const Home = () => {
     const sortBy = sortType.sortProp.replace('-', '');
     const order = sortType.sortProp.includes('-') ? 'asc' : 'desc';
     const category = categoryId > 0 ? `category=${categoryId}` : '';
+    const search = searchValue ? `&search=${searchValue}` : '';
 
     fetch(
-      `https://64295fee5a40b82da4d189a9.mockapi.io/items?${category}&sortBy=${sortBy}&order=${order}`,
+      `https://64295fee5a40b82da4d189a9.mockapi.io/items?page=${currentPage}&limit=4&${category}&sortBy=${sortBy}&order=${order}${search}`,
     )
       .then((res) => res.json())
       .then((arr) => {
@@ -31,7 +37,15 @@ const Home = () => {
         setLoading(false);
         window.scrollTo(0, 0);
       });
-  }, [categoryId, sortType]);
+  }, [categoryId, sortType, searchValue, currentPage]);
+
+  const pizzas = items.map((el) => (
+    <React.Fragment key={el.id}>
+      <PizzaBlock {...el} />
+    </React.Fragment>
+  ));
+
+  const skeletons = [...new Array(6)].map((_, index) => <Skeleton key={index} />);
 
   return (
     <div className="container">
@@ -41,15 +55,8 @@ const Home = () => {
         <Sort value={sortType} onChangeSort={(id) => setsortType(id)} />
       </div>
       <h2 className="content__title">Все пиццы</h2>
-      <div className="content__items">
-        {isLoading
-          ? [...new Array(6)].map((_, index) => <Skeleton key={index} />)
-          : items.map((el) => (
-              <React.Fragment key={el.id}>
-                <PizzaBlock {...el} />
-              </React.Fragment>
-            ))}
-      </div>
+      <div className="content__items">{isLoading ? skeletons : pizzas}</div>
+      <Pagination onChangePage={(number) => setcurrentPage(number)} />
     </div>
   );
 };
