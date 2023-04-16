7fc848e818a10a55a98403ad2b9264efa2d8fc68
SlavaNekrish <vnekrish@mail.ru>
2023-04-16 19:58:17 +0300

    removed to a seporated directory

--- react-ishop/src/redux/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import filter from './slices/filterSlice';
-import cart from './slices/cartSlicee';
-import item from './slices/itemSlice';
-
-export const store = configureStore({
-  reducer: { filter, cart, item },
-});
